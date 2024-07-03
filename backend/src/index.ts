393134256cd53a7c5019bb5c99de907e0a67f396
Ashutosh Sagar <48312681+thecurioussailor@users.noreply.github.com>
2024-07-03 13:26:00 +0530

    setting up backend

---
+++ backend/src/index.ts
@@ -0,0 +1,30 @@
+import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+
+const app = new Hono()
+
+app.post('/api/v1/signup', (c) => {
+  const prisma = new PrismaClient({
+      datasourceUrl: env.DATABASE_URL,
+    }).$extends(withAccelerate())
+  return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/signin', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('Hello Hono!')
+})
+export default app
