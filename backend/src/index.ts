cebdc4ab7f1d58feeb7265721233415d2b3c5724
Ashutosh Sagar <48312681+thecurioussailor@users.noreply.github.com>
2024-08-06 11:50:02 +0530

    cors setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,8 @@ import { withAccelerate } from '@prisma/extension-accelerate'
 import { sign, verify } from 'hono/jwt'
 import { userRouter } from './routes/user'
 import { blogRouter } from './routes/blog'
+import { cors } from 'hono/cors'
+
 
 const app = new Hono<{
   Bindings: {
@@ -11,6 +13,7 @@ const app = new Hono<{
     JWT_SECRET: string
   }
 }>()
+app.use('/api/*', cors())
 app.route("/api/v1/user", userRouter);
 app.route("/api/v1/blog", blogRouter);
 export default app
